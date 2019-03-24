e1dca6ee9b62df78ca69a31f522d5ba96d5a1bde
Radule <master.daca09@gmail.com>
2019-03-24 11:11:48 +0100

    Initial commit

---
+++ tim1/src/main/java/isamrs/tim1/ProjekatApplication.java
@@ -0,0 +1,13 @@
+package isamrs.tim1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatApplication.class, args);
+	}
+
+}
