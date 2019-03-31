68bcf2522f22100882303167e003f97eb868f35d
Dragan Ćulibrk <33953768+draganculibrk9@users.noreply.github.com>
2019-03-31 11:11:36 +0200

    Fixed login bug and completed login backend

--- tim1/src/main/java/isamrs/tim1/controller/AuthenticationController.java
+++ tim1/src/main/java/isamrs/tim1/controller/AuthenticationController.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mobile.device.Device;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import isamrs.tim1.common.DeviceProvider;
 import isamrs.tim1.model.User;
 import isamrs.tim1.model.UserTokenState;
 import isamrs.tim1.security.TokenUtils;
@@ -35,24 +34,24 @@ public class AuthenticationController {
 	@Autowired
 	private CustomUserDetailsService userDetailsService;
 
-	@Autowired
-	private DeviceProvider deviceProvider;
-
 	@RequestMapping(value = "auth/login", method = RequestMethod.POST)
 	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest,
-			HttpServletResponse response, Device device) throws AuthenticationException, IOException {
+			HttpServletResponse response) throws AuthenticationException, IOException {
 
-		final Authentication authentication = authenticationManager
-				.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
-						authenticationRequest.getPassword()));
-
+		final Authentication authentication;
+		try {
+			authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+					authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		} catch (BadCredentialsException e) {
+			return ResponseEntity.ok("Wrong email or password.");
+		}
 		// Ubaci username + password u kontext
 		SecurityContextHolder.getContext().setAuthentication(authentication);
 
 		// Kreiraj token
 		User user = (User) authentication.getPrincipal();
-		String jwt = tokenUtils.generateToken(user.getUsername(), device);
-		int expiresIn = tokenUtils.getExpiredIn(device);
+		String jwt = tokenUtils.generateToken(user.getUsername());
+		int expiresIn = tokenUtils.getExpiredIn();
 
 		// Vrati token kao odgovor na uspesno autentifikaciju
 		return ResponseEntity.ok(new UserTokenState(jwt, expiresIn));
