5578699b17c255539e66c1c159ce35d73344c2fc
Aleksandar Nedaković <aleksaleks123@gmail.com>
2019-04-20 20:15:57 +0200

    Hotel's admin page refactored
    Small fixed on other admins' homepages aswell

--- tim1/src/main/resources/static/hotelAdmin/index.js
+++ tim1/src/main/resources/static/hotelAdmin/index.js
@@ -10,13 +10,60 @@ const getAdditionalServicesURL = "/api/getAdditionalServicesURL";
 const getRoomsURL = "/api/getRooms";
 const getHotelOfAdminURL = "/api/getHotelOfAdmin";
 
+const logoutURL = "../logout";
+const loadUserInfoURL = "../api/getUserInfo";
+const editUserInfoURL = "../api/editUser";
+const changePasswordURL = "../changePassword";
+
 $(document).ready(function() {
 	loadHotel();
+	loadProfileData();
+	setUpTables();
 
+	userEditFormSetUp();
+	$('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+		$($.fn.dataTable.tables(true)).DataTable().columns.adjust();
+	});
+
+	$("#logout").click(function() {
+		document.location.href = logoutURL;
+	});
+
+	$('.edit').click(function() {
+		if ($(this).siblings().first().is('[readonly]')) {
+			$(this).siblings().first().removeAttr('readonly');
+		} else {
+			$(this).siblings().first().prop('readonly', 'true');
+		}
+	});
 })
 
-function setUpMap(latitude, longitude) {
-	var destMap = L.map('mapDiv').setView([ latitude, longitude ], MAP_ZOOM);
+function setUpTables() {
+	$('#roomsTable').DataTable({
+		"paging" : false,
+		"info" : false,
+		"scrollY" : "17vw",
+		"scrollCollapse" : true,
+		"retrieve" : true,
+	});
+	$('#additionalServices').DataTable({
+		"paging" : false,
+		"info" : false,
+		"scrollY" : "17vw",
+		"scrollCollapse" : true,
+		"retrieve" : true,
+	});
+	$('#quickReservationsTable').DataTable({
+		"paging" : false,
+		"info" : false,
+		"scrollY" : "17vw",
+		"scrollCollapse" : true,
+		"retrieve" : true,
+	});
+}
+
+function setUpMap(latitude, longitude, div) {
+	var destMap = L.map(div).setView([ latitude, longitude ], MAP_ZOOM);
 	L.tileLayer(tileLayerURL, {
 		maxZoom : MAX_MAP_ZOOM,
 		id : MAP_ID
@@ -28,20 +75,6 @@ function setUpMap(latitude, longitude) {
 		$("#latitude").val(marker.getLatLng().lat);
 		$("#longitude").val(marker.getLatLng().lng);
 	});
-}
-
-function hideModal() {
-	$("#modalDialog").fadeOut(function() {
-		$("#modalDialog").empty();
-	});
-	$("#overlayDiv").fadeOut();
-}
-function showModal(callback) {
-	$("#modalDialog").fadeIn(function() {
-		if (callback !== undefined)
-			callback();
-	});
-	$("#overlayDiv").fadeIn();
 }
 
 function editHotel(e) {
@@ -69,54 +102,109 @@ function loadHotel() {
 			$("#hotelName").val(data["name"]);
 			$("#hotelGrade").text(data["averageGrade"]);
 			$("#hotelDescription").text(data["description"]);
-			setUpMap(data["latitude"],
-					data["longitude"]);
+			setUpMap(data["latitude"], data["longitude"], 'basicMapDiv');
 			renderAdditionalServices(data["additionalServices"]);
 			renderRooms(data["rooms"]);
+			// renderQuickReservations[data["quickReservations"]];
 		}
 	});
 }
 
-function renderAdditionalServices(list) {
-	var table = $("#additionalServicesTable");
-	$.each(list, function(i, val) {
-		var tr = $("<tr class='tableData'></tr>");
-		tr.append(`<td>${val.name}</td>`);
-		tr.append(`<td>${val.price}</td>`);
-		table.append(tr);
+function renderAdditionalServices(data) {
+	var table = $('#destinationsTable').DataTable();
+	$.each(data, function(i, val) {
+		table.row.add([ val.name, val.price ]).draw(false);
 	});
 }
 
-function renderRooms(list) {
-	var table = $("#roomsTable");
-	$.each(list, function(i, val) {
-		var tr = $("<tr class='tableData'></tr>");
-		tr.append(`<td>${val.roomNumber}</td>`);
-		tr.append(`<td>${val.defaultPriceOneNight}</td>`);
-		tr.append(`<td>${val.numberOfPeople}</td>`);
-		tr.append(`<td>${val.averageGrade}</td>`);
-		table.append(tr);
+function renderFlights(data) {
+	var table = $('#flightsTable').DataTable();
+	$.each(data, function(i, val) {
+		table.row.add(
+				[ val.roomNumber, val.defaultPriceOneNight, val.numberOfPeople,
+						val.averageGrade ]).draw(false);
 	});
 }
 
-function changeContentToRooms() {
-	$(".contentTable").hide();
-	$("#roomsTable").show();
-	$(".active").removeClass("active");
-	$("#roomsTab").addClass("active");
+/* USER PROFILE JS */
+
+function loadProfileData() {
+	let token = getJwtToken("jwtToken");
+	$.ajax({
+		type : 'GET',
+		url : loadUserInfoURL,
+		dataType : "json",
+		headers : createAuthorizationTokenHeader(TOKEN_KEY),
+		success : function(data) {
+			if (data != null) {
+				$('input[name="fname"]').val(data.firstName);
+				$('input[name="lname"]').val(data.lastName);
+				$('input[name="phone"]').val(data.phone);
+				$('input[name="address"]').val(data.address);
+				$('#email').text(data.email);
+			}
+		},
+		error : function(XMLHttpRequest, textStatus, errorThrown) {
+			alert("AJAX ERROR: " + textStatus);
+		}
+	});
 }
 
-function changeContentToAddServices() {
-	$(".contentTable").hide();
-	$("#additionalServicesTable").show();
-	$(".active").removeClass("active");
-	$("#additionalServicesTab").addClass("active");
+$(document).on("click", "#changePasswordButton", function(e) {
+	e.preventDefault();
+	document.location.href = changePasswordURL;
+});
+
+function userFormToJSON(firstName, lastName, phone, address, email) {
+	return JSON.stringify({
+		"firstName" : firstName,
+		"lastName" : lastName,
+		"phoneNumber" : phone,
+		"address" : address,
+		"email" : email
+	});
 }
 
-function changeContentToQuickRes() {
-	$(".contentTable").hide();
-	$("#quickReservationsTable").show();
-	$("#addQuickReservationButton").show();
-	$(".active").removeClass("active");
-	$("#quickReservationsTab").addClass("active");
+function userEditFormSetUp() {
+	$('#userEditForm').on('submit', function(e) {
+		e.preventDefault();
+		let firstName = $('input[name="fname"]').val();
+		let lastName = $('input[name="lname"]').val();
+		let phone = $('input[name="phone"]').val();
+		let address = $('input[name="address"]').val();
+		let email = $('#email').text();
+
+		$.ajax({
+			type : 'PUT',
+			url : editUserInfoURL,
+			contentType : 'application/json',
+			dataType : "html",
+			data : userFormToJSON(firstName, lastName, phone, address, email),
+			success : function(data) {
+				if (data != "") {
+					toastr.options = {
+						"closeButton" : true,
+						"debug" : false,
+						"newestOnTop" : false,
+						"progressBar" : false,
+						"positionClass" : "toast-top-center",
+						"preventDuplicates" : false,
+						"onclick" : null,
+						"showDuration" : "300",
+						"hideDuration" : "1000",
+						"timeOut" : "3000",
+						"extendedTimeOut" : "1000",
+						"showEasing" : "swing",
+						"hideEasing" : "linear",
+						"showMethod" : "fadeIn",
+						"hideMethod" : "fadeOut"
+					}
+					toastr["error"](data);
+				}
+			},
+			error : function(XMLHttpRequest, textStatus, errorThrown) {
+				alert("AJAX ERROR: " + textStatus);
+			}
+		});
+	});
 }
