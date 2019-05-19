04fed36cab3889867b86a8f20d9b56e333147479
Radule <master.daca09@gmail.com>
2019-05-19 12:20:40 +0200

    Backend completed

--- tim1/src/main/java/isamrs/tim1/service/EmailService.java
+++ tim1/src/main/java/isamrs/tim1/service/EmailService.java
@@ -2,6 +2,7 @@ package isamrs.tim1.service;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import isamrs.tim1.model.FlightReservation;
 import isamrs.tim1.model.RegisteredUser;
 import isamrs.tim1.model.User;
 import isamrs.tim1.model.VerificationToken;
@@ -62,6 +64,39 @@ public class EmailService {
 		String content = String.format(
 					"Hello %s %s,\nYour password is: %s",
 					admin.getFirstName(), admin.getLastName(), password);
+		mail.setText(content);
+		mailSender.send(mail);
+	}
+	
+	@Async
+	public void sendMailToFriend(RegisteredUser friend, Long id, String inviter) {
+		SimpleMailMessage mail = new SimpleMailMessage();
+		mail.setTo(friend.getEmail());
+		mail.setFrom(env.getProperty("spring.mail.username"));
+		mail.setSubject("Flight reservation invitation");
+		String content = String.format(
+					"Hello %s %s,\nYour friend %s invited you to flight. "
+					+ "Details are available on link below:\nhttp://localhost:8000/api/flightInvitation?id=%d", 
+					friend.getFirstName(), friend.getLastName(), inviter, id);
+		mail.setText(content);
+		mailSender.send(mail);
+	}
+
+	@Async
+	public void sendFlightReservationMail(RegisteredUser ru, FlightReservation fr) {
+		SimpleMailMessage mail = new SimpleMailMessage();
+		mail.setTo(ru.getEmail());
+		mail.setFrom(env.getProperty("spring.mail.username"));
+		mail.setSubject("Flight reservation");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+		String depTime = sdf.format(fr.getFlight().getDepartureTime());
+		String landTime = sdf.format(fr.getFlight().getLandingTime());
+		String content = String.format(
+					"Hello %s %s,\nYour flight reservation is successfully made."
+					+ "\nStart destination: %s\nEnd destination %s\nDeparture time: %s\nLanding time: %s\n"
+					+ "Number of seats: %d\nPrice %.2f\n", 
+					ru.getFirstName(), ru.getLastName(), fr.getFlight().getStartDestination().getName(), 
+					fr.getFlight().getEndDestination().getName(), depTime, landTime, fr.getPassengerSeats().size(), fr.getPrice());
 		mail.setText(content);
 		mailSender.send(mail);
 	}
--- tim1/src/main/java/isamrs/tim1/service/ReservationService.java
+++ tim1/src/main/java/isamrs/tim1/service/ReservationService.java
@@ -48,6 +48,9 @@ public class ReservationService {
 
 	@Autowired
 	ServiceRepository serviceRepository;
+	
+	@Autowired
+	EmailService mailService;
 
 	public MessageDTO reserveFlight(FlightReservationDTO flightRes) {
 		UserReservation ur = new UserReservation();
@@ -59,6 +62,7 @@ public class ReservationService {
 		userReservationRepository.save(ur);
 		flightReservationRepository.save(fr);
 		userRepository.save(ru);
+		mailService.sendFlightReservationMail(ru, fr);
 		return new MessageDTO("Reservation successfully made.", ToasterType.SUCCESS.toString());
 	}
 
@@ -128,9 +132,14 @@ public class ReservationService {
 		for (PassengerDTO p : passengerList) {
 			String[] idx = flightRes.getSeats()[counter].split("_");
 			price += p.getNumberOfBags() * f.getPricePerBag();
+			int row = Integer.parseInt(idx[0]);
+			int column = Integer.parseInt(idx[1]);
+			if (checkIfSeatIsReserved(f, row, column)) {
+				return new MessageDTO("One of the seats is already reserved.", ToasterType.ERROR.toString());
+			}
 			Seat st = new Seat();
-			st.setRow(Integer.parseInt(idx[0]));
-			st.setColumn(Integer.parseInt(idx[1]));
+			st.setRow(row);
+			st.setColumn(column);
 			if (idx[2].equalsIgnoreCase(PlaneSegmentClass.FIRST.toString().substring(0, 1))) {
 				st.setPlaneSegment(new PlaneSegment(PlaneSegmentClass.FIRST));
 				price += f.getFirstClassPrice();
@@ -148,8 +157,11 @@ public class ReservationService {
 			fr.getPassengerSeats().add(ps);
 			counter++;
 		}
+		for (String email : flightRes.getInvitedFriends()) {
+			inviteFriendToFlight(email, flightRes, f, counter, ru.getEmail());
+			counter++;
+		}
 		fr.setPrice(price);
-
 		ur.setGrade(0);
 		ur.setReservation(fr);
 		ur.setUser(ru);
@@ -157,5 +169,48 @@ public class ReservationService {
 		fr.setUser(ur);
 		f.getAirline().getReservations().add(fr);
 		return new MessageDTO("", ToasterType.SUCCESS.toString());
+	}
+	
+	private boolean checkIfSeatIsReserved(Flight flight, int row, int column) {
+		for (FlightReservation r : flight.getAirline().getReservations()) {
+			if (r.getFlight().getFlightCode().equals(flight.getFlightCode())) {
+				for (PassengerSeat ps : r.getPassengerSeats()) {
+					if (ps.getSeat() != null && (ps.getSeat().getRow() == row && ps.getSeat().getColumn() == column)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	private void inviteFriendToFlight(String email, FlightReservationDTO flightRes, Flight f, int counter, String inviter) {
+		RegisteredUser friend = (RegisteredUser) userRepository.findOneByEmail(email);
+		FlightReservation fRes = new FlightReservation();
+		fRes.setFlight(f);
+		fRes.setDone(false);
+		fRes.setDateOfReservation(new Date());
+		double price = 0.0;
+		String[] idx = flightRes.getSeats()[counter].split("_");
+		Seat st = new Seat();
+		st.setRow(Integer.parseInt(idx[0]));
+		st.setColumn(Integer.parseInt(idx[1]));
+		if (idx[2].equalsIgnoreCase(PlaneSegmentClass.FIRST.toString().substring(0, 1))) {
+			st.setPlaneSegment(new PlaneSegment(PlaneSegmentClass.FIRST));
+			price += f.getFirstClassPrice();
+		} else if (idx[2].equalsIgnoreCase(PlaneSegmentClass.BUSINESS.toString().substring(0, 1))) {
+			st.setPlaneSegment(new PlaneSegment(PlaneSegmentClass.BUSINESS));
+			price += f.getBusinessClassPrice();
+		} else if (idx[2].equalsIgnoreCase(PlaneSegmentClass.ECONOMY.toString().substring(0, 1))) {
+			st.setPlaneSegment(new PlaneSegment(PlaneSegmentClass.ECONOMY));
+			price += f.getEconomyClassPrice();
+		}
+		PassengerSeat ps = new PassengerSeat(new PassengerDTO(friend.getFirstName(), friend.getLastName(), "", 0), st);
+		ps.setReservation(fRes);
+		fRes.setPrice(price);
+		fRes.getPassengerSeats().add(ps);
+		f.getAirline().getReservations().add(fRes);
+		fRes = flightReservationRepository.save(fRes);
+		mailService.sendMailToFriend(friend, fRes.getId(), inviter);
 	}
 }
