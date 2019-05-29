f871c6cb4181f8dbd91c602cd8ddce4703403535
Dragan Ćulibrk <33953768+draganculibrk9@users.noreply.github.com>
2019-05-29 15:18:16 +0200

    Minor changes

--- tim1/src/main/java/isamrs/tim1/common/RequestSender.java
+++ tim1/src/main/java/isamrs/tim1/common/RequestSender.java
@@ -12,8 +12,8 @@ import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.jackson2.JacksonFactory;
 
 public class RequestSender {
-	static final GenericUrl url = new GenericUrl("http://nominatim.openstreetmap.org/reverse?format=json");
-	static final JsonFactory JSON_FACTORY = new JacksonFactory();
+	private static final GenericUrl url = new GenericUrl("http://nominatim.openstreetmap.org/reverse?format=json");
+	private static final JsonFactory JSON_FACTORY = new JacksonFactory();
 
 	public static String getCountry(double latitude, double longitude) throws IOException {
 		HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory(new HttpRequestInitializer() {
@@ -33,7 +33,7 @@ public class RequestSender {
 			GeoResponse response = request.execute().parseAs(GeoResponse.class);
 			return response.getAddress().getCountry();
 		} catch (Exception e) {
-			return "muuu";
+			return "UndefinedCountry";
 		}
 
 	}
