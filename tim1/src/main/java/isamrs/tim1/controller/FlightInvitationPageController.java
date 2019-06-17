ee185fe6d0b29f608d7da13885295930b4863d32
Aleksandar Nedaković <aleksaleks123@gmail.com>
2019-06-17 02:45:58 +0200

    Removed preauthorize annotations from page controllers

--- tim1/src/main/java/isamrs/tim1/controller/FlightInvitationPageController.java
+++ tim1/src/main/java/isamrs/tim1/controller/FlightInvitationPageController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FlightInvitationPageController {
-	@PreAuthorize("hasRole('REGISTEREDUSER')")
 	@GetMapping("/flightInvitation")
 	public String showFlightInvitationPage(Map<String, Object> model){
 		return "flightInvitation/index.html";
